06d800cec88ebca286e417a01fadbb9e1ee72c7a
Malkons <marc.alkons@gmail.com>
2020-07-01 13:00:52 -0700

    added laser with texture

--- public/js/js.js
+++ public/js/js.js
@@ -20,11 +20,14 @@ window.addEventListener("resize", function () {
     camera.updateProjectionMatrix();
 });
 
+//orbit cammera
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 
+//adds colored axis
 var axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
+//logo cube
 var geometry = new THREE.BoxGeometry(2, 2, 2);
 var cubeMaterials = [
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/mountain.png"), side: THREE.DoubleSide }),
@@ -39,6 +42,7 @@ var cube = new THREE.Mesh(geometry, material);
 cube.position.y += 2;
 scene.add(cube);
 
+//ground
 var texture = new THREE.TextureLoader().load("../images/grasslight-big.jpg");
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
@@ -50,7 +54,149 @@ var ground = new THREE.Mesh(
 ground.rotation.x -= Math.PI/2;
 scene.add(ground);
 
+//tableTop
+var geometry =  new THREE.BoxGeometry(3, .5, 4);
+var tableTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(tableTexture);
+var tableTop = new THREE.Mesh(geometry, material);
+tableTop.position.x += 10;
+tableTop.position.y += 3;
+scene.add(tableTop); 
 
+//tableLeg1
+var geometry = new THREE.BoxGeometry(.5, 4, .5);
+var tableTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(tableTexture);
+var tableLeg = new THREE.Mesh(geometry, material);
+tableLeg.position.x += 9;
+tableLeg.position.y += 1;
+tableLeg.position.z += 1.5;
+scene.add(tableLeg);
+
+//tableLeg2
+var geometry = new THREE.BoxGeometry(.5, 4, .5);
+var tableTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(tableTexture);
+var tableLeg = new THREE.Mesh(geometry, material);
+tableLeg.position.x += 9;
+tableLeg.position.y += 1;
+tableLeg.position.z += -1.5;
+scene.add(tableLeg);
+
+//tableLeg3
+var geometry = new THREE.BoxGeometry(.5, 4, .5);
+var tableTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(tableTexture);
+var tableLeg = new THREE.Mesh(geometry, material);
+tableLeg.position.x += 11;
+tableLeg.position.y += 1;
+tableLeg.position.z += -1.5;
+scene.add(tableLeg);
+
+//tableLeg4
+var geometry = new THREE.BoxGeometry(.5, 4, .5);
+var tableTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/woodgrain.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(tableTexture);
+var tableLeg = new THREE.Mesh(geometry, material);
+tableLeg.position.x += 11;
+tableLeg.position.y += 1;
+tableLeg.position.z += 1.5;
+scene.add(tableLeg);
+
+//laser
+var geometry = new THREE.BoxGeometry(2, 1, 3);
+var laserTexture = [
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+	new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("../images/metalbox.jpg"), side: THREE.DoubleSide }),
+];
+var material =  new THREE.MeshFaceMaterial(laserTexture);
+var laser = new THREE.Mesh(geometry, material);
+laser.position.y += 3.5;
+laser.position.x += 10;
+scene.add(laser);
+
+var geometry = new THREE.CylinderGeometry(.30, .30, .5);
+var material = new THREE.MeshBasicMaterial({color: 0x000000});
+var scope = new THREE.Mesh(geometry, material);
+scope.position.y += 3.6;
+scope.position.x += 10;
+scope.position.z += -1.5;
+scope.rotation.x = Math.PI / 2;
+scene.add(scope);
+
+//laser beam
+var material = new THREE.LineBasicMaterial({
+	color: 0xFF0000
+});
+
+var points = [];
+points.push( new THREE.Vector3( 10, 3.6, -10 ) );
+points.push( new THREE.Vector3( 10, 3.6, 0 ) );
+
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+var line = new THREE.Line( geometry, material );
+scene.add( line );
+
+//lense
+var geometry = new THREE.CylinderGeometry(1, 1, .5, 32);
+var material = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, opacity: 0.5});
+var lense = new THREE.Mesh(geometry, material);
+lense.rotation.x = Math.PI / 2;
+lense.position.y += 3.5;
+lense.position.x += 10;
+lense.position.z += -10;
+scene.add(lense);
+
+//lense holder
+var geometry = new THREE.CylinderGeometry(.25, .25, 3, 32);
+var material = new THREE.MeshBasicMaterial({color: 0x000000});
+var lenseHolder = new THREE.Mesh(geometry, material);
+lenseHolder.position.y += 1.25;
+lenseHolder.position.x += 10;
+lenseHolder.position.z += -10
+scene.add(lenseHolder);
+
+//camera position
 camera.position.z = 5;
 camera.lookAt(new THREE.Vector3(0,player.height,0));
 
@@ -58,6 +204,7 @@ var render = function () {
 
     renderer.render(scene, camera);
 };
+
 var update = function () {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.005;
